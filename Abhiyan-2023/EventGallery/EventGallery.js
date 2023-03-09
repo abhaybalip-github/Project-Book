6b72d259ec85a9c6f39545d1584e506db3164373
Abhay Balip <abhay.balip@gmail.com>
2023-03-09 21:52:15 +0530

    event gallery

--- Abhiyan-2023/EventGallery/EventGallery.js
+++ Abhiyan-2023/EventGallery/EventGallery.js
@@ -21,3 +21,22 @@ function dehover_event_card(event_blog,event_poster,event_desc){
     // event_poster.style.zIndex = '-1'
     event_desc.style.visibility = 'hidden'
 }
+
+function hover_event_card_h(event_blog,event_poster,event_desc){
+    event_blog = document.getElementById(event_blog)
+    event_desc = document.getElementById(event_desc)
+    event_poster = document.getElementById(event_poster)
+
+    event_blog.style.height = '60vh'
+    event_poster.style.visibility = 'visible'
+    window.setTimeout(function(){event_desc.style.visibility = 'visible'},300)
+}
+function dehover_event_card_h(event_blog,event_poster,event_desc){
+    event_blog = document.getElementById(event_blog)
+    event_desc = document.getElementById(event_desc)
+    event_poster = document.getElementById(event_poster)
+
+    event_blog.style.height = '30vh'
+    event_poster.style.visibility = 'hidden'
+    event_desc.style.visibility = 'hidden'
+}
