b69f47e1da05012a768417117e8095779327fd36
Abhay Balip <abhay.balip@gmail.com>
2023-03-10 19:15:23 +0530

    event gallery updated

--- Abhiyan-2023/EventGallery/EventGallery.js
+++ Abhiyan-2023/EventGallery/EventGallery.js
@@ -1,42 +1,45 @@
 
 // function for hover effect
-function hover_event_card(event_blog,event_poster,event_desc){
-    event_blog = document.getElementById(event_blog)
-    event_desc = document.getElementById(event_desc)
-    event_poster = document.getElementById(event_poster)
+function hover_event_card(card_no){
+    event_blog = document.getElementsByClassName('event-blog')
+    event_desc = document.getElementsByClassName('event-desc')
+    event_poster = document.getElementsByClassName('event-poster')
 
-    event_blog.style.width = '60vh';
-    event_poster.style.visibility = 'visible'
-    // event_poster.style.zIndex = '1'
-    window.setTimeout(function(){event_desc.style.visibility = 'visible'},300)
+    event_blog[card_no].style.width = '60vh';
+    event_poster[card_no].style.visibility = 'visible'
+    // event_poster[card_no].style.zIndex = '1'
+    window.setTimeout(function(){event_desc[card_no].style.visibility = 'visible'},300)
 }
 // function for de-hover effect
-function dehover_event_card(event_blog,event_poster,event_desc){
-    event_blog = document.getElementById(event_blog)
-    event_desc = document.getElementById(event_desc)
-    event_poster = document.getElementById(event_poster)
+function dehover_event_card(card_no){
+    event_blog = document.getElementsByClassName('event-blog')
+    event_desc = document.getElementsByClassName('event-desc')
+    event_poster = document.getElementsByClassName('event-poster')
 
-    event_blog.style.width = '30vh'
-    event_poster.style.visibility = 'hidden'
-    // event_poster.style.zIndex = '-1'
-    event_desc.style.visibility = 'hidden'
+    event_blog[card_no].style.width = '30vh'
+    event_poster[card_no].style.visibility = 'hidden'
+    // event_poster[card_no].style.zIndex = '-1'
+    event_desc[card_no].style.visibility = 'hidden'
 }
 
-function hover_event_card_h(event_blog,event_poster,event_desc){
-    event_blog = document.getElementById(event_blog)
-    event_desc = document.getElementById(event_desc)
-    event_poster = document.getElementById(event_poster)
+// function for hover effect on h card
+function hover_event_card_h(card_no){
+    event_blog = document.getElementsByClassName('event-blog-h')
+    event_desc = document.getElementsByClassName('event-desc-h')
+    event_poster = document.getElementsByClassName('event-poster-h')
 
-    event_blog.style.height = '60vh'
-    event_poster.style.visibility = 'visible'
-    window.setTimeout(function(){event_desc.style.visibility = 'visible'},300)
+    event_blog[card_no].style.height = '60vh'
+    event_poster[card_no].style.visibility = 'visible'
+    window.setTimeout(function(){event_desc[card_no].style.visibility = 'visible'},300)
 }
-function dehover_event_card_h(event_blog,event_poster,event_desc){
-    event_blog = document.getElementById(event_blog)
-    event_desc = document.getElementById(event_desc)
-    event_poster = document.getElementById(event_poster)
 
-    event_blog.style.height = '30vh'
-    event_poster.style.visibility = 'hidden'
-    event_desc.style.visibility = 'hidden'
+// function for de-hover effect on h card
+function dehover_event_card_h(card_no){
+    event_blog = document.getElementsByClassName('event-blog-h')
+    event_desc = document.getElementsByClassName('event-desc-h')
+    event_poster = document.getElementsByClassName('event-poster-h')
+
+    event_blog[card_no].style.height = '30vh'
+    event_poster[card_no].style.visibility = 'hidden'
+    event_desc[card_no].style.visibility = 'hidden'
 }
