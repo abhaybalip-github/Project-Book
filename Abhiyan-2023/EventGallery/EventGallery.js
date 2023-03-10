800cc72bd5def3c3f777d6d6100253a4f1b665a3
Abhay Balip <abhay.balip@gmail.com>
2023-03-10 22:23:02 +0530

    update

--- Abhiyan-2023/EventGallery/EventGallery.js
+++ Abhiyan-2023/EventGallery/EventGallery.js
@@ -5,10 +5,10 @@ function hover_event_card(card_no){
     event_desc = document.getElementsByClassName('event-desc')
     event_poster = document.getElementsByClassName('event-poster')
 
-    event_blog[card_no].style.width = '60vh';
     event_poster[card_no].style.visibility = 'visible'
-    // event_poster[card_no].style.zIndex = '1'
-    window.setTimeout(function(){event_desc[card_no].style.visibility = 'visible'},300)
+    event_poster[card_no].style.zIndex = '2'
+    event_desc[card_no].style.visibility = 'visible'
+    event_blog[card_no].style.width = '60vh';
 }
 // function for de-hover effect
 function dehover_event_card(card_no){
@@ -16,10 +16,10 @@ function dehover_event_card(card_no){
     event_desc = document.getElementsByClassName('event-desc')
     event_poster = document.getElementsByClassName('event-poster')
 
-    event_blog[card_no].style.width = '30vh'
     event_poster[card_no].style.visibility = 'hidden'
-    // event_poster[card_no].style.zIndex = '-1'
+    event_poster[card_no].style.zIndex = '0'
     event_desc[card_no].style.visibility = 'hidden'
+    event_blog[card_no].style.width = '30vh'
 }
 
 // function for hover effect on h card
@@ -28,11 +28,10 @@ function hover_event_card_h(card_no){
     event_desc = document.getElementsByClassName('event-desc-h')
     event_poster = document.getElementsByClassName('event-poster-h')
 
-    window.clearTimeout()
-
-    event_blog[card_no].style.height = '60vh'
+    event_desc[card_no].style.visibility = 'visible'
+    event_poster[card_no].style.zIndex = '2'
     event_poster[card_no].style.visibility = 'visible'
-    window.setTimeout(function(){event_desc[card_no].style.visibility = 'visible'},300)
+    event_blog[card_no].style.height = '60vh'
 }
 
 // function for de-hover effect on h card
@@ -41,9 +40,8 @@ function dehover_event_card_h(card_no){
     event_desc = document.getElementsByClassName('event-desc-h')
     event_poster = document.getElementsByClassName('event-poster-h')
 
-    window.clearTimeout()
-
-    event_blog[card_no].style.height = '30vh'
     event_poster[card_no].style.visibility = 'hidden'
+    event_poster[card_no].style.zIndex = '0'
     event_desc[card_no].style.visibility = 'hidden'
+    event_blog[card_no].style.height = '30vh'
 }
