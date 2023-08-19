4142e044c91b2a42ef79cef075a48392ab5101d3
Abhay Balip <abhay.balip@gmail.com>
2023-08-19 14:32:57 +0530

    Revert "Add files via upload"
    This reverts commit b4f70c45d8858797130e4bf2214d89f235d07116.

---
+++ React-Prj/login_panel/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Login from './assets/login'
+
+export default class App extends React.Component{
+  constructor(){
+    super()
+  }
+  render(){
+    return (
+      <>
+      <Login/>
+      </>
+    )
+  }
+}
